3cdaa554410c0f669556f476e470e82c4a617bee
Gianni Settino <g@miltonandparc.com>
2014-09-16 23:30:34 -0400

    Initial Commit

---
+++ gslightcontrol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gslightcontrol
+//
+//  Created by Gianni Settino on 2014-09-16.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

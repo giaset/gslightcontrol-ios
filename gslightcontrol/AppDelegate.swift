b2ff99bd020d6d53b5c4475978ffe4faa202b3a5
Gianni Settino <g@miltonandparc.com>
2014-09-17 00:43:34 -0400

    Most of the code now done

--- gslightcontrol/AppDelegate.swift
+++ gslightcontrol/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
-        window!.rootViewController = UINavigationController(rootViewController: ViewController())
+        window!.rootViewController = UINavigationController(rootViewController: ViewController(style: .Grouped))
         
         window!.makeKeyAndVisible()
         
--- gslightcontrol/ViewController.swift
+++ gslightcontrol/ViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     var firebaseRef: Firebase!
+    
+    var colorPickerView: NKOColorPickerView!
+    var onOffSwitch = UISwitch(frame: CGRectZero)
+    var brightnessSlider = UISlider()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,11 +22,68 @@ class ViewController: UIViewController {
         title = "GSLightControl"
         
         firebaseRef = Firebase(url: "https://gslightcontrol.firebaseio.com/")
+        
+        tableView.tableHeaderView = headerView()
+        
+        firebaseRef.childByAppendingPath("on").observeEventType(.Value, withBlock: {
+            snapshot in
+            self.onOffSwitch.on = snapshot.value as Bool
+        })
+        
+        firebaseRef.childByAppendingPath("brightness").observeEventType(.Value, withBlock: {
+            snapshot in
+            self.brightnessSlider.value = snapshot.value as Float
+        })
+    }
+    
+    func headerView() -> UIView {
+        var didChangeColorBlock: NKOColorPickerDidChangeColorBlock = { color in println(color) }
+        
+        colorPickerView = NKOColorPickerView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 150), color: UIColor.redColor(), andDidChangeColorBlock: didChangeColorBlock)
+        
+        return colorPickerView
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Options"
+        case 1:
+            return "Brightness"
+        default:
+            return ""
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var CellIdentifier = (indexPath.section == 0) ? "SwitchCell" : "SliderCell"
+        var cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as? UITableViewCell
+        
+        if (cell == nil) {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: CellIdentifier)
+            cell!.selectionStyle = .None
+            
+            if (indexPath.section == 0) {
+                cell!.textLabel!.text = "On/Off"
+                cell!.accessoryView = onOffSwitch
+            } else {
+                brightnessSlider.frame = CGRect(x: 20, y: 5, width: cell!.contentView.frame.width, height: brightnessSlider.frame.height)
+                brightnessSlider.minimumValue = 0
+                brightnessSlider.maximumValue = 1
+                cell!.contentView.addSubview(brightnessSlider)
+            }
+            
+        }
+        
+        return cell!
     }
 
 }
-
-/*myRootRef.observeEventType(.Value, withBlock: {
-snapshot in
-println("\(snapshot.name) -> \(snapshot.value)")
-})*/

82b0e377f08f593db2ff3e73d0494da8d343feb1
Gianni Settino <g@miltonandparc.com>
2014-09-17 00:03:49 -0400

    Import Firebase framework

--- gslightcontrol/ViewController.swift
+++ gslightcontrol/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firebaseRef: Firebase!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "GSLightControl"
+        
+        firebaseRef = Firebase(url: "https://gslightcontrol.firebaseio.com/")
     }
 
 }
 
+/*myRootRef.observeEventType(.Value, withBlock: {
+snapshot in
+println("\(snapshot.name) -> \(snapshot.value)")
+})*/

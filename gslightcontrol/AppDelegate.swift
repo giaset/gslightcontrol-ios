e1af695950ecbe9700f811b69791e15cc82945cb
Gianni Settino <g@miltonandparc.com>
2014-09-16 23:57:26 -0400

    Delete Main storyboard and use AppDelegate instead

--- gslightcontrol/AppDelegate.swift
+++ gslightcontrol/AppDelegate.swift
@@ -12,6 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        window!.rootViewController = UINavigationController(rootViewController: ViewController())
+        
+        window!.makeKeyAndVisible()
+        
+        return true
+    }
 
 }
 
